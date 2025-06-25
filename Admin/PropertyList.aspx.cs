852209913595769cf431c011074058ffeda1f186
YourFavoutireDev <nguyenhuulong312ds@email.com>
2025-06-25 14:11:36 +0700

    Add: Admin Site

---
+++ Admin/PropertyList.aspx.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CodeUtility;
+using System.IO;
+
+public partial class Admin_PropertyList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ucMessage.HideAll();
+            ShowPreviousMessage();
+            LoadMainCategoryData();
+            LoadCategoryData();
+            LoadData();
+        }
+    }
+
+    public void LoadData()
+    {
+        //Lấy ID và mID từ url
+        int ID = Request.QueryString["id"].ToInt();
+
+
+        //Vào DB tìm 1 item phù hợp
+        DBEntities db = new DBEntities();
+        var item = db.Properties.Where(x => x.PropertyID == ID).FirstOrDefault();
+
+        //Nếu ko có thì báo lỗi, kết thúc
+        if (item == null)
+        {
+            return;
+        }
+        //Hiển thị từng giá trị lên các control
+        DropDownList_MainCategory.SelectedValue = item.PropertyCategoryID.ToSafetyString();
+        ListBox_Category.SelectedValue = item.PropertyID.ToSafetyString();
+
+        input_ID.Value = item.PropertyID.ToSafetyString();
+        input_Code.Value = item.Code;
+        input_Title.Value = item.Title;
+        input_PropertyValue.Value = item.PropertyValue;
+        textarea_Description.Value = item.Description;
+        input_Position.Value = item.Position.ToSafetyString();
+
+        a_Avatar.HRef = item.Avatar;
+        img_Avatar.Src = item.Avatar;
+
+        if (item.Status == true)
+        {
+            radio_Lock.Checked = false;
+            radio_Active.Checked = true;
+        }
+        else
+        {
+            radio_Active.Checked = false;
+            radio_Lock.Checked = true;
+        }
+    }
+
+    public void LoadMainCategoryData()
+    {
+        DBEntities db = new DBEntities();
+        //Query dữ liệu
+        var query = db.PropertyCategories.OrderBy(x => x.Position).Select(x => new { x.PropertyCategoryID, x.Title });
+        //Lấy danh sách query đó
+        var data = query.ToList();
+        //Cấu hình hiển thị
+        DropDownList_MainCategory.DataValueField = "PropertyCategoryID";//Khóa chính
+        DropDownList_MainCategory.DataTextField = "Title";//Tên cột cần hiển thị
+
+        DropDownList_MainCategoryLeft.DataValueField = "PropertyCategoryID";//Khóa chính
+        DropDownList_MainCategoryLeft.DataTextField = "Title";//Tên cột cần hiển thị
+
+        //Đổ dữ liệu
+        DropDownList_MainCategory.DataSource = data;
+        DropDownList_MainCategoryLeft.DataSource = data;
+        //Hiển thị
+        DropDownList_MainCategory.DataBind();
+        DropDownList_MainCategoryLeft.DataBind();
+
+        //Chọn lại phần tử đã click trước đó
+        int mID = Request.QueryString["mid"].ToInt();
+        if (mID > 0)
+        {
+            DropDownList_MainCategoryLeft.SelectedValue = mID.ToSafetyString();
+            DropDownList_MainCategory.SelectedValue = mID.ToSafetyString();
+        }
+
+    }
+
+    public void LoadCategoryData()
+    {
+        int mID = DropDownList_MainCategoryLeft.SelectedValue.ToInt();
+        //Kết nối DB
+        DBEntities db = new DBEntities();
+        //Query dữ liệu
+        var query = db.Properties.OrderBy(x => x.Position).Where(x => x.PropertyCategoryID == mID).Select(x => new { x.PropertyID, x.Title });
+        //Lấy danh sách query đó
+        var data = query.ToList();
+        //Cấu hình hiển thị
+        ListBox_Category.DataValueField = "PropertyID";//Khóa chính
+        ListBox_Category.DataTextField = "Title";//Tên cột cần hiển thị
+        //Đổ dữ liệu
+        ListBox_Category.DataSource = data;
+        //Hiển thị
+        ListBox_Category.DataBind();
+
+        //Tạo thêm một item mặc định
+        ListItem item = new ListItem();
+        item.Value = string.Empty;
+        item.Text = ".:Chọn loại hình ảnh cấp con:.";
+
+        //Chèn item mặc định vào đầu danh sách dropdown
+        ListBox_Category.Items.Insert(0, item);//Vị trí và chèn thằng nào vào dropdown
+
+    }
+
+    public void SearchData(string type = "", string messages = "")
+    {
+        //Lấy các giá trị tìm kiếm
+        int ID = ListBox_Category.SelectedValue.ToInt();
+        int mID = DropDownList_MainCategoryLeft.SelectedValue.ToInt();
+        string title = input_Title.Value.Trim();
+
+        //Tạo url trang hiện tại kèm theo điều kiện search
+        string url = "~/Admin/PropertyList.aspx?mid={0}&id={1}&messagetype={2}&message={3}";
+        url = url.StringFormat(mID, ID, type, messages);
+
+        //Chuyển về trang đích
+        Response.Redirect(url);
+    }
+
+    private void ShowPreviousMessage()
+    {
+        //Hiển thị thông báo từ lần trước
+        String messageType = Request.QueryString["messagetype"].ToSafetyString();
+        string message = Request.QueryString["message"].ToSafetyString();
+
+        if (messageType == "success")
+        {
+            ucMessage.ShowSuccess(message);
+        }
+
+        else if (messageType == "warning")
+        {
+            ucMessage.ShowWarning(message);
+        }
+
+        else if (messageType == "info")
+        {
+            ucMessage.ShowInfo(message);
+        }
+
+        else if (messageType == "error")
+        {
+            ucMessage.ShowError(message);
+        }
+    }
+
+    protected void DropDownList_MainCategoryLeft_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Load dữ liệu con
+        LoadCategoryData();
+        SearchData();
+    }
+
+    protected void DropDownList_MainCategory_SelectedIndexChanged(object sender, EventArgs e)
+    {
+    }
+
+    protected void ListBox_Category_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SearchData();
+    }
+
+    protected void LinkButton_Add_Click(object sender, EventArgs e)
+    {
+        ListBox_Category.SelectedIndex = 0;
+        SearchData();
+    }
+
+    protected void LinkButton_Delete_Click(object sender, EventArgs e)
+    {
+        //Lấy ID của tài khoản cần xóa
+        int ID = Request.QueryString["id"].ToInt();
+
+        if (ID <= 0)
+        {
+            ucMessage.ShowError("Vui lòng chọn một phần tử để xóa");
+            return;
+        }
+
+        //Tìm một tài khoản có ID như trên
+        DBEntities db = new DBEntities();
+        var item = db.Properties.Where(x => x.PropertyID == ID).FirstOrDefault();
+
+        //Nếu không tìm thấy thì load lại dữ liệu
+        if (item == null)
+        {
+            SearchData("error", "Dữ liệu không còn tồn tại, vui lòng kiểm tra lại");
+            return;
+        }
+
+        //Xóa dữ liệu được chọn
+        db.Properties.Remove(item);
+        try
+        {
+            //Lưu lại DB
+            db.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            ucMessage.ShowError("Không thể xóa do ràng buộc dữ liệu, vui lòng xóa dữ liệu liên quan trước");
+            return;
+        }
+
+        //Load lại danh sách
+        LoadCategoryData();
+        //Load lại trang
+        SearchData("success", "Đã xóa dữ liệu");
+        return;
+
+    }
+
+    protected void LinkButton_Save_Click(object sender, EventArgs e)
+    {
+        //Ẩn thông báo
+        ucMessage.HideAll();
+
+        //Lấy ID từ Url xuống
+        int ID = Request.QueryString["id"].ToInt();
+
+        //Lấy giá trị các ô nhập
+        int mainCatID = DropDownList_MainCategory.SelectedValue.ToInt();
+        int position = input_Position.Value.ToInt();
+        string code = input_Code.Value.Trim();
+        string title = input_Title.Value.Trim();
+        string propertyValue = input_PropertyValue.Value.Trim();
+        string description = textarea_Description.Value.Trim();
+        bool status = radio_Active.Checked;
+
+        //Upload hình
+        string avatar = string.Empty;
+        string thumb = string.Empty;
+
+        if (FileUpload_Avatar.FileName != string.Empty)
+        {
+            //Kiểm tra đuôi hình hợp lệ
+            string validExtension = ".jpg.jpeg.png.gif.bmp.ico";
+            string fileExtension = Path.GetExtension(FileUpload_Avatar.FileName.ToLower());
+            if (!validExtension.Contains(fileExtension))
+            {
+                ucMessage.ShowError("Hình không hợp lệ, loại hình hỗ trợ: jpg, jpeg, png, gif, bmp, ico");
+                return;
+            }
+
+            //Kiểm tra dung lượng file <= 3Mb
+            int validSize = 1024 * 1024 * 3;
+            int fileSize = FileUpload_Avatar.FileBytes.Length;
+            if (fileSize > validSize)
+            {
+                ucMessage.ShowError("Dung lượng hình cần <= 3Mb");
+                return;
+            }
+
+            Exception error = null;
+            UploadUtility uploadUtility = new UploadUtility();
+            uploadUtility.FileUpload = FileUpload_Avatar;
+            uploadUtility.FolderSave = "~/fileuploads/Property";
+            uploadUtility.FullMaxWidth = 1000;
+            uploadUtility.ThumbMaxWidth = 400;
+            uploadUtility.MaxFileSize = 1024 * 1024 * 3;
+            uploadUtility.AutoGenerateFileName = true;
+            uploadUtility.UploadImage(ref avatar, ref thumb, ref error);
+        }
+
+        //Kiểm tra title hợp lệ
+        if (title == string.Empty)
+        {
+            ucMessage.ShowError("Vui lòng nhập tiêu đề");
+            return;
+        }
+
+        //Nếu có ID, thì Update (Save)
+        if (ID > 0)
+        {
+            //Tìm 1 item thích hợp
+            DBEntities db = new DBEntities();
+            var item = db.Properties.Where(x => x.PropertyID == ID).FirstOrDefault();
+
+            //Nếu ko có, thì báo lỗi, kết thúc
+            if (item == null)
+            {
+                ucMessage.ShowError("Dữ liệu này không còn tồn tại");
+                return;
+            }
+
+            //Cập nhật giá trị mới
+            item.PropertyCategoryID = mainCatID;
+            item.Code = code;
+            item.Title = title;
+            item.PropertyValue = propertyValue;
+            item.Description = description;
+            item.Status = status;
+            item.CreateBy = SessionUtility.AdminUsername;
+
+            //Nếu có nhập vị trí, thì lưu vị trí
+            if (position > 0)
+                item.Position = position;
+
+            //Nếu có úp hình, thì mới cập nhật hình mới
+            if (avatar != string.Empty)
+            {
+                item.Avatar = avatar;
+                item.Thumb = thumb;
+            }
+
+            //Lưu lại
+            db.SaveChanges();
+
+            //Tạo url trang hiện tại kèm theo điều kiện search
+            string url = "~/Admin/PropertyList.aspx?mid={0}&id={1}&messagetype={2}&message={3}";
+            url = url.StringFormat(item.PropertyCategoryID, item.PropertyID, "success", "Đã cập nhật dữ liệu");
+
+            //Chuyển về trang đích
+            Response.Redirect(url);
+        }
+
+        //Ngược lại, thì Insert (Add)
+        else
+        {
+            //Tạo 1 item mới có kiểu là bảng cần thêm
+            Property item = new Property();
+
+            //Lần lượt gán từng giá trị vào mỗi ô của item
+            item.PropertyCategoryID = mainCatID;
+            item.Code = code;
+            item.Title = title;
+            item.PropertyValue = propertyValue;
+            item.Description = description;
+            item.Status = status;
+            item.CreateBy = SessionUtility.AdminUsername;
+
+            //Nếu có nhập vị trí, thì lưu vị trí
+            if (position > 0)
+                item.Position = position;
+
+            //Nếu có úp hình, thì mới cập nhật hình mới
+            if (avatar != string.Empty)
+            {
+                item.Avatar = avatar;
+                item.Thumb = thumb;
+            }
+
+            //Thêm vào bảng
+            DBEntities db = new DBEntities();
+            db.Properties.Add(item);
+
+            //Lưu DB
+            db.SaveChanges();
+
+            //Tạo url trang hiện tại kèm theo điều kiện search
+            string url = "~/Admin/PropertyList.aspx?mid={0}&id={1}&messagetype={2}&message={3}";
+            url = url.StringFormat(item.PropertyCategoryID, item.PropertyID, "success", "Đã cập nhật dữ liệu");
+
+            //Chuyển về trang đích
+            Response.Redirect(url);
+        }
+    }
+
+
+}
---
+++ Admin/javascript/script.js
@@ -0,0 +1,174 @@
+﻿////$(function () {
+////    var htmlKeyValue =
+////        `<tr>
+////                <td >
+////                    {0}
+////                 </td>
+////                <td>
+////                    <select class="property"></select>
+////                 </td>
+////                 <td class='text-center'>
+////                    <button class='btn btn-primary add-above-this-property tipb' title='Chèn dòng phía trên' type='button'>
+////                        <i class='fa fa-caret-square-o-up fa-lg'></i>
+////                    </button>
+////                    <button class='btn btn-primary add-below-this-property tipb' title='Chèn dòng phía dưới' type='button'>
+////                        <i class='fa fa-caret-square-o-down fa-lg'></i>
+////                    </button>
+////                    <button class='btn btn-danger this-remove tipb' title='Xóa dòng hiện tại' type='button'>
+////                        <i class='fa fa-trash fa-lg'></i>
+////                    </button>
+////                 </td>
+////            </tr>
+////        `;
+////    //<input type='text' onkeyup='SaveProperty(this);' placeholder='Ví dụ: Trọng lượng' />
+////    //<input type='text' onkeyup='SaveProperty(this);' class='tab-add-property' placeholder='Ví dụ: 5Kg' />
+////    loadDataProperty().then(res => {
+////        var listCategory = JSON.parse(res);
+////        console.log(listCategory);
+////        var tempOption = "<option value='{0}'>{1}</option>";
+////        var allOption = "";
+////        listCategory.map(item => {
+////            allOption += tempOption.replace("{0}", item.ID).replace("{1}", item.Title);
+////        });
+////        var selectTemplate = "<select class='property-category'><option value='0'>Chọn loại thuộc tính</option>" + allOption + "</select>"
+
+////        htmlKeyValue = htmlKeyValue.replace("{0}", selectTemplate);
+
+////        //====================== ADD PROPERTY =========================
+////        //Load property
+////        $(".table-property").on("change", ".property-category", function () {
+////            let self = this;
+////            //thêm vào
+////            $(this).closest("tr").find(".property").empty()//Clear option cũ đi
+////            var catId = $(this).val();
+////            loadPropertyByMainId(catId).then(res => {
+////                var lsProperty = JSON.parse(res);
+////                var tempOptionProperty = "<option value='{0}'>{1}</option>";
+////                var allOptionProperty = "";
+////                lsProperty.map(itemProperty => {
+////                    allOptionProperty += tempOptionProperty.replace("{0}", itemProperty.ID).replace("{1}", itemProperty.Title);
+////                })
+////                $(this).closest("tr").find(".property").append(allOptionProperty);
+
+////                //Save
+////                SaveProperty(self);
+////            })
+////        });
+
+////        $(".table-property").on("change", ".property", function () {
+////            //Save
+////            SaveProperty(self);
+////        })
+
+////        //Add property
+////        $(".add-property").click(function () {
+////            //thêm vào
+////            $(this).closest(".table-property").find("tbody").append(htmlKeyValue);
+////        });
+
+////        //Add below this property//thêm dưới
+////        $(".table-property").on("click", ".add-below-this-property", function () {
+////            //thêm vào
+////            $(this).closest("tr").after(htmlKeyValue);
+////        });
+
+////        //Add above this property//thêm trên
+////        $(".table-property").on("click", ".add-above-this-property", function () {
+////            //thêm vào
+////            $(this).closest("tr").before(htmlKeyValue);
+////        });
+
+////        //tab add property
+////        $(".table-property").on("keydown", ".tab-add-property:last", function (event) {
+////            //keyCode: mã của phím|| which: thể hiện phím được bấm
+////            if (event.keyCode === 9 || event.which === 9) {
+////                $(this).closest("tr").after(htmlKeyValue);
+////            }
+////        });
+
+////        //=================== REMOVE PROPERTY =======================
+////        //Remove  property
+////        $(".remove-last-property").click(function () {
+////            //Giờ ví dụ nó về 0 thì thông báo k cho remove nữa
+////            if ($(".table-property > tbody >tr").length == 0) {
+////                alert("Thuộc tính đã trống! Vui lòng thêm thuộc tính");
+////                return false;
+////            }
+////            $(".table-property > tbody > tr").last().remove();
+////        });
+
+////        //This remove
+////        $(".table-property").on("click", ".this-remove", function () {
+
+////            ErrorSaveClick($(this));
+////            $(this).closest("tr").remove();
+////            SaveProperty();
+
+
+////        });
+
+
+////    });
+   
+
+////});
+
+//////Kiểm tra lỗi rỗng thuộc tính khi nhán nút lưu
+////function ErrorSaveClick(e) {
+////    var flag = true;
+////    var selectInput = e.closest(".table-property").find("input[type='text']");
+////    var saveIndex = 0;
+////    selectInput.each(function (index) {
+////        var valProperty = $(this).val();
+
+////        if (valProperty == "") {
+////            saveIndex++;
+////            $(this).attr("style", "border: 1px solid red");
+
+////            $(".h3-description").click();
+
+////            if (saveIndex == 1) {
+////                $(this).focus();
+////            }
+////            flag = false;
+////        }
+////    });
+////    if (!flag) {
+////        alert("Vui lòng nhập đầy đủ thông tin cấu hình, không được để trống ô nào !");
+
+////        return false;
+////    }
+
+////    return true;
+////}
+
+//////Hàm lưu thuoc tính
+////function SaveProperty(e) {
+////    //khai báo biến chứa chuổi
+////    $(e).attr("style", "");
+////    var descriptions = "";
+////    var selectInput = $(".table-property > tbody select.property");
+////    let arr = [];
+////    selectInput.each(function (index) {
+////       var valProperty = $(this).val();
+////        //if ((index + 1) % 2 == 0)
+////        //    descriptions += valProperty + ";";
+////        //else
+////        //    descriptions += valProperty + ":";
+       
+////        arr.push({ ProductID: $(".product-id").val(),PropertyID: valProperty });
+////    });
+        
+
+////    /*descriptions = descriptions.slice(0, descriptions.length - 1)*///bỏ dấu ; ở cuối chuỗi
+////    $(".input_Property").val(JSON.stringify(arr));
+////}
+
+////function loadDataProperty() {
+////   return $.get("/ServiceUtility.asmx/GetAllPropertyCategoryProduct");
+////}
+
+
+////function loadPropertyByMainId(id) {
+////    return $.get("/ServiceUtility.asmx/GetAllPropertyByMainID?id="+id);
+////}
